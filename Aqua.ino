2723746f7c1aa2af2d5b36ff2c1cf1a3427df914
QXL5489 <sboganov@gmail.com>
2018-03-14 22:31:07 +0100

    blink every second

--- Aqua.ino
+++ Aqua.ino
@@ -1,15 +1,24 @@
 #include "Arduino.h"
 #include "RTClock.h"
+
+#define LED_PIN PC13
+
 RTClock rt(RTCSEL_LSE); // initialise
 USBSerial SerialDebug;
+
+void everySecond() {
+	digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+}
 
 void setup()
 {
 	SerialDebug.begin(Serial);
+	pinMode(LED_PIN, OUTPUT);
+	rt.attachSecondsInterrupt(everySecond);
 }
 
 
 void loop()
 {
-
+	delay(5000);
 }
--- sloeber.ino.cpp
+++ sloeber.ino.cpp
@@ -2,12 +2,13 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2018-03-14 22:15:10
+//This file has been generated on 2018-03-14 22:29:34
 
 #include "Arduino.h"
 #include "Arduino.h"
 #include "RTClock.h"
 
+void everySecond() ;
 void setup() ;
 void loop() ;
 
